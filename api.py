53028faa4a2b43c7b2641a5b8c32ce056a5dd41b
Oli Rowan <olirowan@gmail.com>
2017-11-17 13:08:16 +0000

    Added variable in URL for rating

--- api.py
+++ api.py
@@ -13,55 +13,15 @@ app = Flask(__name__)
 
 mongo = PyMongo(app)
 
+@app.route('/rating/<int:number>/', methods=['GET'])
 
-@app.route('/one/', methods=['GET'])
+def get_rating(number):
 
-def get_one_star():
-
-    review = db.reviews.find_one({'rating': 1})
+    review = db.reviews.find_one({'rating': number})
     review['_id'] = str(review['_id'])
 
     return jsonify({'result': [review]})
 
-
-@app.route('/two/', methods=['GET'])
-
-def get_two_star():
-
-    review = db.reviews.find_one({'rating': 2})
-    review['_id'] = str(review['_id'])
-
-    return jsonify({'result': [review]})
-
-
-@app.route('/three/', methods=['GET'])
-
-def get_three_star():
-
-    review = db.reviews.find_one({'rating': 3})
-    review['_id'] = str(review['_id'])
-
-    return jsonify({'result': [review]})
-
-
-@app.route('/four/', methods=['GET'])
-
-def get_four_star():
-
-    review = db.reviews.find_one({'rating': 4})
-    review['_id'] = str(review['_id'])
-
-    return jsonify({'result': [review]})
-
-
-@app.route('/five/', methods=['GET'])
-
-def get_five_star():
-
-    review = db.reviews.find_one({'rating': 5})
-    review['_id'] = str(review['_id'])
-
-    return jsonify({'result': [review]})
 
 
 if __name__ == '__main__':
