2299b4e1b959efb76ccf4f7bc07edd108e422f48
Oli Rowan <olirowan@gmail.com>
2017-11-17 12:53:12 +0000

    Added API path for each star rating.

--- api.py
+++ api.py
@@ -16,9 +16,51 @@ mongo = PyMongo(app)
 
 @app.route('/one/', methods=['GET'])
 
-def get_one_review():
+def get_one_star():
+
+    review = db.reviews.find_one({'rating': 1})
+    review['_id'] = str(review['_id'])
+
+    return jsonify({'result': [review]})
+
+
+@app.route('/two/', methods=['GET'])
+
+def get_two_star():
+
+    review = db.reviews.find_one({'rating': 2})
+    review['_id'] = str(review['_id'])
+
+    return jsonify({'result': [review]})
+
+
+@app.route('/three/', methods=['GET'])
+
+def get_three_star():
+
+    review = db.reviews.find_one({'rating': 3})
+    review['_id'] = str(review['_id'])
+
+    return jsonify({'result': [review]})
+
+
+@app.route('/four/', methods=['GET'])
+
+def get_four_star():
+
+    review = db.reviews.find_one({'rating': 4})
+    review['_id'] = str(review['_id'])
+
+    return jsonify({'result': [review]})
+
+
+@app.route('/five/', methods=['GET'])
+
+def get_five_star():
+
     review = db.reviews.find_one({'rating': 5})
     review['_id'] = str(review['_id'])
+
     return jsonify({'result': [review]})
 
 
