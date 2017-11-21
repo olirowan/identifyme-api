23c803965e411edfc18e4bb7bb036e8924e7f7d4
Oli Rowan <olirowan@gmail.com>
2017-11-21 14:43:04 +0000

    Added a call to retrieve restaurants based on cuisine type.

--- api.py
+++ api.py
@@ -4,8 +4,6 @@ from flask import Flask
 from flask import jsonify
 from flask import request
 
-
-
 client = MongoClient("mongodb://192.168.0.39:27017")
 db = client.business
 
@@ -13,29 +11,45 @@ app = Flask(__name__)
 mongo = PyMongo(app)
 
 
+
 # API GET request to return one restaurant with the rating included in the URL.
 @app.route('/rating/<int:number>/', methods=['GET'])
 
 def get_rating(number):
-
     review = db.reviews.find_one({'rating': number})
-    review['_id'] = str(review['_id']) # This ID needs to be converted to a string due to the JSON requirements.
+    review['_id'] = str(review['_id'])  # This ID needs to be converted to a string due to the JSON requirements.
 
     return jsonify({'result': [review]})
+
+
+
+# API GET request to return all restaurants with the cuisine included in the URL.
+@app.route('/cuisine/<cuisine>/', methods=['GET'])
+
+def get_cuisine(cuisine):
+
+    output = []
+    for resturant in db.reviews.find({'cuisine': cuisine}):
+        resturant['_id'] = str(resturant['_id'])  # This ID needs to be converted to a string due to the JSON requirements.
+
+        output.append({'_id': resturant['_id'], 'cuisine': resturant['cuisine'], 'name': resturant['name'],
+                       'rating': resturant['rating']})
+
+    return jsonify({'result': [output]})
+
 
 
 # Another GET request to return every entry in the table.
 @app.route('/rating', methods=['GET'])
 
 def get_all_ratings():
+    output = []
+    for resturant in db.reviews.find():
+        resturant['_id'] = str(resturant['_id'])
+        output.append({'_id': resturant['_id'], 'cuisine': resturant['cuisine'], 'name': resturant['name'],
+                       'rating': resturant['rating']})
 
-  output = []
-  for resturant in db.reviews.find():
-
-    resturant['_id'] = str(resturant['_id'])
-    output.append({'_id' : resturant['_id'], 'cuisine' : resturant['cuisine'], 'name' : resturant['name'], 'rating' : resturant['rating']})
-
-  return jsonify({'result' : output})
+    return jsonify({'result': output})
 
 
 
@@ -44,18 +58,18 @@ def get_all_ratings():
 
 def add_rating():
 
-  cuisine = request.json['cuisine']
-  name = request.json['name']
-  rating = request.json['rating']
+    cuisine = request.json['cuisine']
+    name = request.json['name']
+    rating = request.json['rating']
 
-  review_id = db.reviews.insert({'cuisine': cuisine, 'name': name, 'rating': rating})
+    review_id = db.reviews.insert({'cuisine': cuisine, 'name': name, 'rating': rating})
 
-  new_review = db.reviews.find_one({'_id': review_id })
-  new_review['_id'] = str(new_review['_id'])
-  output = {'_id' : new_review['_id'], 'cuisine' : new_review['cuisine'], 'name' : new_review['name'], 'rating' : new_review['rating']}
+    new_review = db.reviews.find_one({'_id': review_id})
+    new_review['_id'] = str(new_review['_id'])
+    output = {'_id': new_review['_id'], 'cuisine': new_review['cuisine'], 'name': new_review['name'],
+              'rating': new_review['rating']}
 
-  return jsonify({'result' : output})
-
+    return jsonify({'result': output})
 
 
 if __name__ == '__main__':
