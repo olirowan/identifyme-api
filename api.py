5c68a13aae0518fc5743c07717c902d19f28c699
Oli Rowan <olirowan@gmail.com>
2017-11-21 14:03:05 +0000

    Allows requests for all entries

--- api.py
+++ api.py
@@ -37,5 +37,25 @@ def get_all_ratings():
   return jsonify({'result' : output})
 
 
+
+
+@app.route('/rating', methods=['POST'])
+
+def add_rating():
+
+  cuisine = request.json['cuisine']
+  name = request.json['name']
+  rating = request.json['rating']
+
+  review_id = db.reviews.insert({'cuisine': cuisine, 'name': name, 'rating': rating})
+
+  new_review = db.reviews.find_one({'_id': review_id })
+  new_review['_id'] = str(new_review['_id'])
+  output = {'_id' : new_review['_id'], 'cuisine' : new_review['cuisine'], 'name' : new_review['name'], 'rating' : new_review['rating']}
+
+  return jsonify({'result' : output})
+
+
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
