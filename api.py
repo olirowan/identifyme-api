e576a10c4800eb0d6d2f7368cc1764e5b81bf4fa
Oli Rowan <olirowan@gmail.com>
2017-11-21 14:11:34 +0000

    Allows POST, and comments included.

--- api.py
+++ api.py
@@ -10,25 +10,26 @@ client = MongoClient("mongodb://192.168.0.39:27017")
 db = client.business
 
 app = Flask(__name__)
-
 mongo = PyMongo(app)
 
+
+# API GET request to return one restaurant with the rating included in the URL.
 @app.route('/rating/<int:number>/', methods=['GET'])
 
 def get_rating(number):
 
     review = db.reviews.find_one({'rating': number})
-    review['_id'] = str(review['_id'])
+    review['_id'] = str(review['_id']) # This ID needs to be converted to a string due to the JSON requirements.
 
     return jsonify({'result': [review]})
 
 
+# Another GET request to return every entry in the table.
 @app.route('/rating', methods=['GET'])
 
 def get_all_ratings():
 
   output = []
-
   for resturant in db.reviews.find():
 
     resturant['_id'] = str(resturant['_id'])
@@ -38,7 +39,7 @@ def get_all_ratings():
 
 
 
-
+# A POST to the API that allows a user to add an entry to the DB.
 @app.route('/rating', methods=['POST'])
 
 def add_rating():
